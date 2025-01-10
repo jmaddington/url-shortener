acdb0d449d4e756f3ecf8330c7a4cd9011e009bf
Jonathan Addington <jonathan.addington@jmaddington.com>
2025-01-10 09:05:22 -0500

    feat: Update database schema and templates to support new fields and improve UI

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 -- clicks definition
 
-CREATE TABLE clicks (
+CREATE TABLE IF NOT EXISTS clicks (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     short_link TEXT NOT NULL,
     ip_address TEXT NOT NULL,
@@ -10,19 +10,24 @@ CREATE TABLE clicks (
     FOREIGN KEY (short_link) REFERENCES links(short_link) ON DELETE CASCADE
 );
 
-CREATE INDEX idx_clicks_short_link ON clicks(short_link);
-CREATE INDEX idx_clicks_ip_address ON clicks(ip_address);
-CREATE INDEX clicks_short_link_IDX ON clicks (short_link);
+CREATE INDEX IF NOT EXISTS idx_clicks_short_link ON clicks(short_link);
+CREATE INDEX IF NOT EXISTS idx_clicks_ip_address ON clicks(ip_address);
+CREATE INDEX IF NOT EXISTS clicks_short_link_IDX ON clicks (short_link);
 
 -- links definition
 
-CREATE TABLE links (
+CREATE TABLE IF NOT EXISTS links (
     short_link TEXT PRIMARY KEY,
     target_url TEXT NOT NULL,
     is_file INTEGER NOT NULL DEFAULT 0,
     filename TEXT,
     created_by TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-, description TEXT, expires_at TEXT, guid_required TEXT, basic_auth_user TEXT, basic_auth_pass TEXT);
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    description TEXT,
+    expires_at TEXT,
+    guid_required TEXT,
+    basic_auth_user TEXT,
+    basic_auth_pass TEXT
+);
 
-CREATE INDEX links_short_link_IDX ON links (short_link);
+CREATE INDEX IF NOT EXISTS links_short_link_IDX ON links (short_link);
